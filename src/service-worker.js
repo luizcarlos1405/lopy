b65bb3c70e6aebd9f02063ee4f23f0478e1b3e92
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-03-31 11:10:52 -0300

    service worker fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -77,13 +77,13 @@ self.addEventListener('fetch', event => {
         // for pages, you might want to serve a shell `service-worker-index.html` file,
         // which Sapper has generated for you. It's not right for every
         // app, but if it's right for yours then uncomment this section
-        if (
-          !cachedAsset &&
-          url.origin === self.origin &&
-          routes.find(route => route.pattern.test(url.pathname))
-        ) {
-          return caches.match('/service-worker-index.html');
-        }
+        // if (
+        //   !cachedAsset &&
+        //   url.origin === self.origin &&
+        //   routes.find(route => route.pattern.test(url.pathname))
+        // ) {
+        //   return caches.match('/service-worker-index.html');
+        // }
 
         return cachedAsset || fetchAndCache(event.request);
       })()
