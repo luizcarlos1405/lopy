60192b4f535ead2ed6055e221447458fec22ef69
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-03-31 11:25:32 -0300

    staging

--- src/server.js
+++ src/server.js
@@ -6,12 +6,15 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const url = dev ? '/' : 'lopy-staging';
+
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    url,
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
