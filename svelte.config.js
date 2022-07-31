8c54ec353000999cc49def1e5e4020b1fa16bb1f
Luiz de Oliveira <luizcarlos1405@gmail.com>
2022-07-30 20:10:07 -0400

    use vite to build instead

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
   kit: {
     prerender: {
       default: true,
