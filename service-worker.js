96c79cb4ba132ce7fb5ee5b6ca561c6b1cad4c28
Luiz de Oliveira <luizcarlos1405@gmail.com>
2022-07-30 20:03:01 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,84 +1,74 @@
-const timestamp = {
+const i = {
   toString: () => {
     throw new Error("`timestamp` has been removed from $service-worker. Use `version` instead");
   }
-};
-const build = [
-  "/_app/start-f11f0999.js",
-  "/_app/pages/__layout.svelte-d0b0eadb.js",
-  "/_app/assets/pages/__layout.svelte-e52c889b.css",
-  "/_app/pages/__error.svelte-503c7bd8.js",
-  "/_app/assets/pages/__error.svelte-c5da8346.css",
-  "/_app/pages/index.svelte-8cf9366a.js",
-  "/_app/assets/pages/index.svelte-e9433e1a.css",
-  "/_app/pages/transactions.svelte-04decc3b.js",
-  "/_app/assets/pages/transactions.svelte-3b7c7fb7.css",
-  "/_app/pages/envelope/edit/_id_.svelte-cd216c73.js",
-  "/_app/assets/pages/envelope/edit/_id_.svelte-3e00504f.css",
-  "/_app/pages/envelope/_id_.svelte-90ebd03c.js",
-  "/_app/assets/pages/envelope/_id_.svelte-0bed5e77.css",
-  "/_app/pages/settings.svelte-d40a3447.js",
-  "/_app/chunks/vendor-c0a95dcb.js",
-  "/_app/chunks/singletons-d1fb5791.js",
-  "/_app/chunks/Envelope-f11463df.js",
-  "/_app/chunks/BottomNavigation-334748b7.js",
-  "/_app/assets/BottomNavigation-a22f806f.css",
-  "/_app/chunks/notionStore-26f6cc43.js",
-  "/_app/chunks/stores-2000987a.js",
-  "/_app/chunks/longpress-a7a6c534.js",
-  "/_app/chunks/EnvelopeTransactions-262cad4c.js",
-  "/_app/assets/EnvelopeTransactions-74bb8ea9.css"
-];
-const files = [
+}, m = [
+  "/_app/immutable/start-aca5b326.js",
+  "/_app/immutable/pages/__layout.svelte-36900d43.js",
+  "/_app/immutable/assets/__layout-7872798b.css",
+  "/_app/immutable/pages/__error.svelte-46665dcd.js",
+  "/_app/immutable/assets/__error-222277ab.css",
+  "/_app/immutable/pages/envelope/_id_.svelte-49313f31.js",
+  "/_app/immutable/assets/[id]-ad3ef020.css",
+  "/_app/immutable/pages/envelope/edit/_id_.svelte-94a62ab7.js",
+  "/_app/immutable/assets/[id]-98577166.css",
+  "/_app/immutable/pages/index.svelte-2b97a019.js",
+  "/_app/immutable/assets/index-e4405f24.css",
+  "/_app/immutable/pages/settings.svelte-159a0674.js",
+  "/_app/immutable/pages/transactions.svelte-bb6aa0be.js",
+  "/_app/immutable/assets/transactions-fab3a9ea.css",
+  "/_app/immutable/chunks/index-2c5b7e63.js",
+  "/_app/immutable/chunks/index-a5fb40b1.js",
+  "/_app/immutable/chunks/singletons-eca981c1.js",
+  "/_app/immutable/chunks/stores-6241235a.js",
+  "/_app/immutable/chunks/EnvelopeTransactions-166505be.js",
+  "/_app/immutable/assets/EnvelopeTransactions-9a6e8b8e.css",
+  "/_app/immutable/chunks/Envelope-87461f17.js",
+  "/_app/immutable/chunks/Trash2Icon-4a809962.js",
+  "/_app/immutable/chunks/notionStore-61880af3.js",
+  "/_app/immutable/chunks/longpress-f0a75fa9.js",
+  "/_app/immutable/chunks/BottomNavigation-5e4ce5cd.js",
+  "/_app/immutable/assets/BottomNavigation-d370ba13.css"
+], r = [
   "/favicon.ico",
   "/logo-192.png",
   "/logo-512.png",
   "/logo-maskable-512.png",
   "/manifest.json"
-];
-const worker = self;
-const FILES = `cache${timestamp}`;
-const to_cache = build.concat(files);
-const staticAssets = new Set(to_cache);
-worker.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(FILES).then((cache) => cache.addAll(to_cache)).then(() => {
-    worker.skipWaiting();
-  }));
+], t = self, o = `cache${i}`, p = m.concat(r), u = new Set(p);
+t.addEventListener("install", (s) => {
+  s.waitUntil(
+    caches.open(o).then((e) => e.addAll(p)).then(() => {
+      t.skipWaiting();
+    })
+  );
 });
-worker.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then(async (keys) => {
-    for (const key of keys) {
-      if (key !== FILES)
-        await caches.delete(key);
-    }
-    worker.clients.claim();
-  }));
+t.addEventListener("activate", (s) => {
+  s.waitUntil(
+    caches.keys().then(async (e) => {
+      for (const a of e)
+        a !== o && await caches.delete(a);
+      t.clients.claim();
+    })
+  );
 });
-async function fetchAndCache(request) {
-  const cache = await caches.open(`offline${timestamp}`);
+async function h(s) {
+  const e = await caches.open(`offline${i}`);
   try {
-    const response = await fetch(request);
-    cache.put(request, response.clone());
-    return response;
-  } catch (err) {
-    const response = await cache.match(request);
-    if (response)
-      return response;
-    throw err;
+    const a = await fetch(s);
+    return e.put(s, a.clone()), a;
+  } catch (a) {
+    const c = await e.match(s);
+    if (c)
+      return c;
+    throw a;
   }
 }
-worker.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET" || event.request.headers.has("range"))
+t.addEventListener("fetch", (s) => {
+  if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const url = new URL(event.request.url);
-  const isHttp = url.protocol.startsWith("http");
-  const isDevServerRequest = url.hostname === self.location.hostname && url.port !== self.location.port;
-  const isStaticAsset = url.host === self.location.host && staticAssets.has(url.pathname);
-  const skipBecauseUncached = event.request.cache === "only-if-cached" && !isStaticAsset;
-  if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
-    event.respondWith((async () => {
-      const cachedAsset = isStaticAsset && await caches.match(event.request);
-      return cachedAsset || fetchAndCache(event.request);
-    })());
-  }
+  const e = new URL(s.request.url), a = e.protocol.startsWith("http"), c = e.hostname === self.location.hostname && e.port !== self.location.port, n = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !n;
+  a && !c && !l && s.respondWith(
+    (async () => n && await caches.match(s.request) || h(s.request))()
+  );
 });
