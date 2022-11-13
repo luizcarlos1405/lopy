c76984e73d6fa81b269d939f980a205ac65da583
Luiz de Oliveira <luizcarlos1405@gmail.com>
2022-11-13 19:21:49 -0400

    Skip more lines each step of deploy

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ echo "\n==== Building ===="
 npm run build
 
 # Github usually ignores files and folders that start with "_". This file makes it not do that.
-echo "\n==== Ensuring github configurations ===="
+echo "\n\n==== Ensuring github configurations ===="
 touch build/.nojekyll
 echo "Added .nojekyll file to make github serve files that start with _"
 echo "lopy.suaveware.dev" > build/CNAME
 echo "Added CNAME file with the url to point github pages to"
 
-echo "Which version to bump? [major | minor | patch] https://docs.npmjs.com/cli/v7/commands/npm-version"
+echo "\n\nWhich version to bump? [major | minor | patch] https://docs.npmjs.com/cli/v7/commands/npm-version"
 read versionName
 
-echo "\nnpm version $versionName"
+echo "\n\nnpm version $versionName"
 npm version $versionName
 
-echo "\n==== Deploying with gh-pages ===="
+echo "\n\n==== Deploying with gh-pages ===="
 gh-pages -d build --dotfiles
