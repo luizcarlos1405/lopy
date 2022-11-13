9c361ddb70a553f37edfe359827ec1d6de314ed1
Luiz de Oliveira <luizcarlos1405@gmail.com>
2022-11-13 19:15:14 -0400

    Exits deploy script if any command fails

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
-echo "Which version to bump? [major | minor | patch] https://docs.npmjs.com/cli/v7/commands/npm-version"
-read versionName
-
-echo "\nnpm version $versionName"
-npm version $versionName
+# Exits if any command fails
+set -e
 
 echo "\n==== Building ===="
-yarn build
+npm run build
 
 # Github usually ignores files and folders that start with "_". This file makes it not do that.
 echo "\n==== Ensuring github configurations ===="
@@ -13,6 +10,12 @@ touch build/.nojekyll
 echo "Added .nojekyll file to make github serve files that start with _"
 echo "lopy.suaveware.dev" > build/CNAME
 echo "Added CNAME file with the url to point github pages to"
+
+echo "Which version to bump? [major | minor | patch] https://docs.npmjs.com/cli/v7/commands/npm-version"
+read versionName
+
+echo "\nnpm version $versionName"
+npm version $versionName
 
 echo "\n==== Deploying with gh-pages ===="
 gh-pages -d build --dotfiles
