f89ff99d91ae566bd97ed2a874ad9ed8331339fc
Luiz de Oliveira <luizcarlos1405@gmail.com>
2023-06-25 16:48:34 -0400

    Use adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,7 @@ const config = {
     }),
   ],
   kit: {
-		adapter: adapter(),
+    adapter: adapter(),
   },
 };
 
